8298d59a3b74a6b2072b6dc9a13c74b0360dbfc0
projects-api <hogwartsedx.official@gmail.com>
2024-06-15 11:33:53 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ const { sendWelcomeEmail } = require('./mailer');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://hogwarts-api-14jun.onrender.com/api/auth/google/callback",
+    callbackURL: "https://hogwartsedx-api-15jun.onrender.com/api/auth/google/callback",
     passReqToCallback: true
   },
   async (req, token, tokenSecret, profile, done) => {
